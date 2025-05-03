523312c7c980e30c74dc2e236a6c48659f501c63
Haycel <arzachalvin@gmail.com>
2025-05-03 20:42:05 +0700

    Add files via upload

---
+++ transaksi_sekolah/src/Tampilan/Koneksi.java
@@ -0,0 +1,28 @@
+package Tampilan;
+import java.sql.*;
+/**
+ *
+ * @author Haycel arza chalvin
+ */
+public class Koneksi {
+    private Connection koneksi;
+    public Connection connect(){
+        try{
+              Class.forName("com.mysql.jdbc.Driver");
+              System.out.println("berhasil konek");
+        }
+        catch(ClassNotFoundException ex){
+            System.out.println("gagal koneksi"+ex);
+        }
+        String url = "jdbc:mysql://localhost:3306/penjualan";
+        try{
+            koneksi = DriverManager.getConnection(url,"root","");
+            System.out.println("berhasil koneksi database");
+        }
+        catch (SQLException ex) {
+            System.out.println("gagal koneksi database"+ex);
+        }
+        return koneksi;
+    }
+    
+}
